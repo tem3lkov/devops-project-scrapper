cef71b6c4967b6ce0aca25dcc4e7493161c9f00b
Todor Temelkov <115074162+tem3lkov@users.noreply.github.com>
2025-12-25 15:34:01 +0200

    Update timer.py

--- backend/utils/timer.py
+++ backend/utils/timer.py
@@ -1,25 +1,28 @@
 import time
 from typing import Optional
 
+
 class Elapsed:
     _start: float
     _elapsed: Optional[float] = None
 
-    # called when the context manager is entered
     def __enter__(self) -> "Elapsed":
+        """Called when the context manager is entered."""
         self._start = time.perf_counter()
         return self
 
-    # called when the context manager is exited
     def __exit__(self, *_exc) -> None:
+        """Called when the context manager is exited."""
         self._elapsed = time.perf_counter() - self._start
 
-    # called when the object is called
     def __call__(self) -> float:
+        """Return the elapsed time in seconds (rounded to 3 decimals)."""
         now = time.perf_counter()
-        return round(
-            (self._elapsed if self._elapsed is not None else now - self._start), 3
+        duration = (
+            self._elapsed if self._elapsed is not None else now - self._start
         )
+        return round(duration, 3)
 
-def elapsed() -> Elapsed:    
+
+def elapsed() -> Elapsed:
     return Elapsed()
